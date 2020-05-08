3fafef5dfdf3ef741b37da629023c4e257b8aafe
CristianX <thecristianx@hotmail.com>
2020-05-07 19:15:53 -0500

    Implementando bottomNavigationBar en botones_page

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -23,6 +23,8 @@ class BotonesPage extends StatelessWidget {
           )
         ],
       ),
+      // Navegación en la parte de abajo
+      bottomNavigationBar: _bottonNavigationBar(context)
     );
   }
 
@@ -100,4 +102,32 @@ class BotonesPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget _bottonNavigationBar( BuildContext context ) {
+    // Widget con tema personalizado solo aplicado al navigation (no es global)
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme.copyWith( caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0) ) )
+      ),
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+          icon: Icon( Icons.calendar_today, size: 30.0 ),
+          title: Container()
+          ),
+          BottomNavigationBarItem(
+          icon: Icon( Icons.bubble_chart, size: 30.0 ),
+          title: Container()
+          ),
+          BottomNavigationBarItem(
+          icon: Icon( Icons.supervised_user_circle, size: 30.0 ),
+          title: Container()
+          ),
+        ]
+      ),
+    );
+  }
+
 }
